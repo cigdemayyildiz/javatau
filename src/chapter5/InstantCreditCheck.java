ba1c5ef009dd02e68916ec4b51a28b0fd591e4ae
Cigdem <cigdemrespect@gmail.com>
2022-01-03 07:21:50 -0600

    InstantCreditCheck

--- src/chapter5/InstantCreditCheck.java
+++ src/chapter5/InstantCreditCheck.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
  * Write an 'instance credit check' program that approves anyone who makes more
  * than $25,000 and has a credit score of 700 or better. Reject all others.
  */
+
 public class InstantCreditCheck {
 
     static double requiredSalary = 25000;
