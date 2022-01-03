86decba559be1a6e4e204b98a260eef82e5eeb7a
Cigdem <cigdemrespect@gmail.com>
2022-01-03 07:20:17 -0600

    LetterSearch

--- src/chapter4/LetterSearch.java
+++ src/chapter4/LetterSearch.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
  * Loop Break
  * Search a String to determine if it contains the letter 'A'.
  */
+
 public class LetterSearch {
 
     public static void main(String[] args) {
